39a6158c53752319910b87df41f8a84e363cdfad
Helen Bailey <hakbailey@gmail.com>
2020-09-21 16:05:07 -0400

    Complete JPAL ingest to include all metadata fields in our Dataset model

--- hoard/sources/jpal.py
+++ hoard/sources/jpal.py
@@ -1,10 +1,24 @@
 from bs4 import BeautifulSoup  # type: ignore
 import requests
-from typing import List, Iterator
+from typing import Any, Dict, Iterator, Optional
 
 
 from hoard.client import OAIClient
-from hoard.models import Author, Contact, Dataset, Description
+from hoard.models import (
+    Author,
+    Contact,
+    Contributor,
+    Dataset,
+    Description,
+    Distributor,
+    GrantNumber,
+    Keyword,
+    OtherId,
+    Producer,
+    Publication,
+    Series,
+    TimePeriodCovered,
+)
 
 
 class JPAL:
@@ -25,53 +39,164 @@ class JPAL:
 
 
 def create_from_dataverse_json(data: dict) -> Dataset:
-    fields = data["datasetVersion"]["metadataBlocks"]["citation"]["fields"]
-    title = [x["value"] for x in fields if x["typeName"] == "title"][0]
-    authors = [get_authors(x["value"]) for x in fields if x["typeName"] == "author"][0]
-    contacts = [
-        get_contacts(x["value"]) for x in fields if x["typeName"] == "datasetContact"
-    ][0]
-    description = [
-        get_descriptions(x["value"]) for x in fields if x["typeName"] == "dsDescription"
-    ][0]
-    subjects = [[v for v in x["value"]] for x in fields if x["typeName"] == "subject"][
-        0
+    kwargs: Dict[str, Any] = {}
+
+    # Dataset fields
+    kwargs["alternativeURL"] = data.get("persistentUrl")
+    kwargs["distributionDate"] = data.get("publicationDate")
+    kwargs["distributors"] = [
+        Distributor(
+            distributorName="The Abdul Latif Jameel Poverty Action Lab Dataverse",
+            distributorURL="https://dataverse.harvard.edu/dataverse/jpal",
+        )
     ]
+    kwargs["license"] = data["datasetVersion"].get("license")
+    kwargs["termsOfUse"] = data["datasetVersion"].get("termsOfUse")
 
-    return Dataset(
-        title=title,
-        authors=authors,
-        contacts=contacts,
-        description=description,
-        subjects=subjects,
-    )
+    # Citation fields
+    fields = data["datasetVersion"]["metadataBlocks"]["citation"]["fields"]
+
+    for field in fields:
+        if field["typeName"] == "author":
+            kwargs["authors"] = [
+                Author(
+                    authorAffiliation=v["authorAffiliation"]["value"],
+                    authorIdentifier=get_optional_value(v, "authorIdentifier"),
+                    authorIdentifierScheme=get_optional_value(
+                        v, "authorIdentifierScheme"
+                    ),
+                    authorName=v["authorName"]["value"],
+                )
+                for v in field["value"]
+            ]
+
+        elif field["typeName"] == "datasetContact":
+            kwargs["contacts"] = [
+                Contact(
+                    datasetContactAffiliation=get_optional_value(
+                        v, "datasetContactAffiliation"
+                    ),
+                    datasetContactEmail=v["datasetContactEmail"]["value"],
+                    datasetContactName=v["datasetContactName"]["value"],
+                )
+                for v in field["value"]
+            ]
+
+        elif field["typeName"] == "contributor":
+            kwargs["contributors"] = [
+                Contributor(
+                    contributorName=get_optional_value(v, "contributorName"),
+                    contributorType=get_optional_value(v, "contributorType"),
+                )
+                for v in field["value"]
+            ]
+
+        elif field["typeName"] == "dsDescription":
+            kwargs["description"] = [
+                Description(
+                    dsDescriptionDate=get_optional_value(v, "dsDescriptionDate"),
+                    dsDescriptionValue=v["dsDescriptionValue"]["value"],
+                )
+                for v in field["value"]
+            ]
+
+        elif field["typeName"] == "distributor":
+            kwargs["distributors"].extend(
+                [
+                    Distributor(
+                        distributorName=get_optional_value(v, "distributorName"),
+                        distributorURL=get_optional_value(v, "distributorURL"),
+                    )
+                    for v in field["value"]
+                ]
+            )
+
+        elif field["typeName"] == "grantNumber":
+            kwargs["grantNumbers"] = [
+                GrantNumber(
+                    grantNumberAgency=get_optional_value(v, "grantNumberAgency"),
+                    grantNumberValue=get_optional_value(v, "grantNumberValue"),
+                )
+                for v in field["value"]
+            ]
+
+        elif field["typeName"] == "keyword":
+            kwargs["keywords"] = [
+                Keyword(keywordValue=get_optional_value(v, "keywordValue"),)
+                for v in field["value"]
+            ]
+
+        elif field["typeName"] == "kindOfData":
+            kwargs["kindOfData"] = field["value"]
+
+        elif field["typeName"] == "language":
+            kwargs["language"] = field["value"]
+
+        elif field["typeName"] == "notesText":
+            kwargs["notesText"] = field["value"]
+
+        elif field["typeName"] == "otherId":
+            kwargs["otherIds"] = [
+                OtherId(
+                    otherIdAgency=get_optional_value(v, "otherIdAgency"),
+                    otherIdValue=get_optional_value(v, "otherIdValue"),
+                )
+                for v in field["value"]
+            ]
+
+        elif field["typeName"] == "producer":
+            kwargs["producers"] = [
+                Producer(
+                    producerName=get_optional_value(v, "producerName"),
+                    producerURL=get_optional_value(v, "producerURL"),
+                )
+                for v in field["value"]
+            ]
+
+        elif field["typeName"] == "productionPlace":
+            kwargs["productionPlace"] = field["value"]
+
+        elif field["typeName"] == "publication":
+            kwargs["publications"] = [
+                Publication(
+                    publicationCitation=get_optional_value(v, "publicationCitation"),
+                    publicationIDNumber=get_optional_value(v, "publicationIDNumber"),
+                    publicationIDType=get_optional_value(v, "publicationIDType"),
+                    publicationURL=get_optional_value(v, "publicationURL"),
+                )
+                for v in field["value"]
+            ]
+
+        elif field["typeName"] == "series":
+            kwargs["series"] = Series(
+                seriesName=get_optional_value(field["value"], "seriesName"),
+                seriesInformation=get_optional_value(
+                    field["value"], "seriesInformation"
+                ),
+            )
+
+        elif field["typeName"] == "subject":
+            kwargs["subjects"] = field["value"]
+
+        elif field["typeName"] == "timePeriodCovered":
+            kwargs["timePeriodsCovered"] = [
+                TimePeriodCovered(
+                    timePeriodCoveredStart=get_optional_value(
+                        v, "timePeriodCoveredStart"
+                    ),
+                    timePeriodCoveredEnd=get_optional_value(v, "timePeriodCoveredEnd"),
+                )
+                for v in field["value"]
+            ]
+
+        elif field["typeName"] == "title":
+            kwargs["title"] = field["value"]
+
+    return Dataset(**kwargs)
 
 
-def get_authors(values: dict) -> List[Author]:
-    authors = []
-    for v in values:
-        author = Author(
-            authorName=v["authorName"]["value"],
-            authorAffiliation=v["authorAffiliation"]["value"],
-        )
-        authors.append(author)
-    return authors
-
-
-def get_contacts(values: dict) -> List[Contact]:
-    contacts = []
-    for v in values:
-        contact = Contact(
-            datasetContactName=v["datasetContactName"]["value"],
-            datasetContactEmail=v["datasetContactEmail"]["value"],
-        )
-        contacts.append(contact)
-    return contacts
-
-
-def get_descriptions(values: dict) -> List[Description]:
-    descriptions = []
-    for v in values:
-        description = Description(dsDescriptionValue=v["dsDescriptionValue"]["value"],)
-        descriptions.append(description)
-    return descriptions
+def get_optional_value(d: dict, k: str) -> Optional[str]:
+    if d.get(k) is not None:
+        return d[k]["value"]
+    else:
+        return None
